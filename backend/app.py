5a959de03810f6f1973e886f73a34ee89c7b5222
znb899 <83007857+znb899@users.noreply.github.com>
2024-05-14 09:47:11 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,90 @@
-from flask import Flask, Response
-from flask_cors import CORS
-from time import sleep
+"""Example LangChain server exposes a conversational retrieval agent.
 
-app = Flask(__name__)
+Relevant LangChain documentation:
 
-def dummy_text_generator():
-    text = 'This is a sample text to test the app but could be any stream of text'
-    for t in text.split():
-        yield t + ' '
-        sleep(3)
+* Creating a custom agent: https://python.langchain.com/docs/modules/agents/how_to/custom_agent
+* Streaming with agents: https://python.langchain.com/docs/modules/agents/how_to/streaming#custom-streaming-with-events
+* General streaming documentation: https://python.langchain.com/docs/expression_language/streaming
 
-@app.route('/api/completion', methods=['POST'])
-def stream():
-    return Response(dummy_text_generator())
+**ATTENTION**
+1. To support streaming individual tokens you will need to use the astream events
+   endpoint rather than the streaming endpoint.
+2. This example does not truncate message history, so it will crash if you
+   send too many messages (exceed token length).
+3. The playground at the moment does not render agent output well! If you want to
+   use the playground you need to customize it's output server side using astream
+   events by wrapping it within another runnable.
+4. See the client notebook it has an example of how to use stream_events client side!
+"""
 
-if __name__ == '__main__':
-    app.run(port=3000)  
+import pysqlite3
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+from fastapi import FastAPI
+
+from typing import Any, List, Union
+
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad.openai_tools import (
+    format_to_openai_tool_messages,
+)
+from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.tools import tool
+from langchain_core.utils.function_calling import format_tool_to_openai_tool
+from langchain_openai import AzureChatOpenAI
+
+from langserve import add_routes
+from langserve.pydantic_v1 import BaseModel, Field
+from langchain_core.messages import SystemMessage
+
+import json
+from formation import catalogue
+
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from typing import Any, Dict 
+
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="Spin up a simple api server using LangChain's Runnable interfaces",
+)
+
+# We need to add these input/output schemas because the current AgentExecutor
+# is lacking in schemas.
+class Input(BaseModel):
+    input: str
+    # The field extra defines a chat widget.
+    # Please see documentation about widgets in the main README.
+    # The widget is used in the playground.
+    # Keep in mind that playground support for agents is not great at the moment.
+    # To get a better experience, you'll need to customize the streaming output
+    # for now.
+    chat_history: List
+
+
+class Output(BaseModel):
+    output: Any
+
+
+from main import app as llm_app
+
+# Adds routes to the app for using the chain under:
+# /invoke
+# /batch
+# /stream
+# /stream_events
+add_routes(
+    app,
+    llm_app.with_types(input_type=Input, output_type=Output),
+    path="/api",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
