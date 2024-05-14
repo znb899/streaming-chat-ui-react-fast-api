e4d88861d03355023c3e7dc5089122598e658bd1
znb899 <83007857+znb899@users.noreply.github.com>
2024-05-14 09:48:03 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { RemoteRunnable } from "@langchain/core/runnables/remote";
 import './App.css';
+import showdown from 'showdown';
 
 function App() {
   const [isActive, setActive] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [chatHistory, setChatHistory] = useState([]);
   const inputRef = useRef();
   const submissionsContainerRef = useRef();
+
+  const remoteChain = new RemoteRunnable({
+    url: "/api",
+    options: {
+      timeout: 60000,
+    },
+  });
+
+  const converter = new showdown.Converter();
 
   const handleSubmit = async () => {
     let text = inputRef.current.innerText;
@@ -25,24 +37,32 @@ function App() {
       botDiv.id = 'bot-message';
 
       try {
-        const response = await fetch('/api/completion', {
-          method: 'POST',
-          body: JSON.stringify({'message': text}),
-          headers: {
-           'Content-Type': 'application/json'
-          }
+        const stream = await remoteChain.streamEvents({
+          input: text,
+          chat_history: chatHistory
+        },{
+          version: "v1",
         });
-        
-        const reader = response.body.getReader();
+
         var botText = '';
-        while (true) {
-          const {done, value} = await reader.read();
-          if (done) break;
-          let resText = new TextDecoder().decode(value);
-          botText += resText;
-          botDiv.innerHTML = `<p class='persona'>Bot</p><p class='message'>${botText}</p>`;
-          submissionsContainerRef.current.appendChild(botDiv); // Append bot's message to the submissions container
+        for await (const event of stream) {
+          if (event["event"] === 'on_chat_model_stream'){
+            var content = event["data"]["chunk"].content;
+            if (content){
+              botText += content;
+              botDiv.innerHTML = `<p class='persona'>Bot</p><p class='message'>${converter.makeHtml(botText)}</p>`;
+              submissionsContainerRef.current.appendChild(botDiv); // Append bot's message to the submissions container
+            }
+          }
         }
+
+        setChatHistory([...chatHistory, {
+          'direction': 'outgoing',
+          'content': text
+        }, {
+          'direction': 'incoming',
+          'content': botText
+        }]);
         
       } catch (error) {
         console.error(error);
